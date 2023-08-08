cdd533acd6fa2d8f8a40324e82a91330c7b0f7d4
Andrew213455 <woodring.andrew@yahoo.com>
2023-08-08 11:30:38 -0400

    backend working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,11 @@ import * as functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
 import playlistRouter from "./routes/playlistRouter";
+import newsFeedRouter from "./routes/newsFeedRouter";
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", playlistRouter);
+app.use("/", newsFeedRouter);
 
 export const api = functions.https.onRequest(app);
